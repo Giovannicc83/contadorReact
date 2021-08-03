db80eea52e40c971b6a15c2cf86d2fb330ff454c
Giovanni <geovannycc@gmail.com>
2021-08-02 22:31:29 -0600

    Agregando proyecto React Contador terminado

---
+++ src/Contador.js
@@ -0,0 +1,53 @@
+import { useState} from "react";
+
+
+
+//Opcion 1
+// const Contador = ({valor}) => {
+    
+//     const [conteo, setConteo] = useState(valor);
+//     const handleIncrease = () =>{
+//         setConteo (conteo + 1);
+//     }
+
+//     const handleDecrease = () =>{
+//         setConteo (conteo - 1);
+//     }
+
+//     const handleReset = () =>{
+//         setConteo (valor);
+//     }
+
+//     return (
+//         <>
+//             <h1>Contador</h1>
+//             <h2>{conteo}</h2>
+//             <button onClick={handleIncrease} >+</button>
+            
+//             <button onClick={handleReset} >RESET</button>
+            
+//             <button onClick={handleDecrease} >-</button>
+//         </>
+//     )
+// }
+
+// export default Contador;
+
+
+
+
+//Opcion 2
+const Contador = ({valor}) => {
+    
+    const [conteo, setConteo] = useState(valor);
+    return (
+        <>
+            <h1>Contador</h1>
+            <h2>{conteo}</h2>
+            <button onClick={() => setConteo (conteo + 1)} >+</button>            
+            <button onClick={() => setConteo (conteo - 1)} >RESET</button>            
+            <button onClick={() => setConteo (conteo - 1)} >-</button>
+        </>
+    )
+}
+export default Contador;
